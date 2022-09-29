fa6f48aeff7f27ac9585ae1f5c0f879e870eec75
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-09-29 21:30:19 +0300

    Add files via upload

---
+++ lesson20BlackJack/Main.java
@@ -0,0 +1,8 @@
+package com.company.lesson20BlackJack;
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck =new Deck();
+        System.out.println(deck);
+    }
+}
