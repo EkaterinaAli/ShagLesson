a6a4aeaf169fafb0d71164ded25924460cbf3545
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-10-04 21:10:35 +0300

    Add files via upload

---
+++ lesson20BlackJack/lesson20BlackJack/Dealer.java
@@ -0,0 +1,17 @@
+package com.company.lesson20BlackJack;
+
+public class Dealer extends Hand{
+
+    @Override
+    public void play(Deck deck) throws InterruptedException {
+        takeOneCard(deck);
+        takeOneCard(deck);
+        while (calcScore() < 17 ){
+            System.out.println("Dealer hand: " + hand + "dealer score:" + calcScore());
+            takeOneCard(deck);
+            Thread.sleep(1000);
+        }
+        System.out.println("Dealer hand: " + hand + "dealer score:" + calcScore());
+
+    }
+}
---
+++ lesson20BlackJack/lesson20BlackJack/Main.java
@@ -0,0 +1,8 @@
+package com.company.lesson20BlackJack;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Game game = new Game();
+        game.play();
+    }
+}
