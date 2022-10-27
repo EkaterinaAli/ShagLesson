20d7fefda6851486b382bf7333c7fcdc0eaa6c35
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-10-27 20:04:18 +0300

    Add files via upload

--- lesson27_BST/BinaryTree.java
+++ lesson27_BST/BinaryTree.java
@@ -3,9 +3,10 @@ package com.company.lesson27_BST;
 public interface BinaryTree <T>{
     boolean isEmpty();
     void add(T data);
-    Node <T> findMin(Node <T> rood);
+    T findMin();
     T findMax();
     void delete(T data);
     boolean contains(T data);
+    void inorder();
 
 }
---
+++ lesson27_BST/Test.java
@@ -0,0 +1,21 @@
+package com.company.lesson27_BST;
+
+public class Test {
+    public static void main(String[] args) {
+        BST <Integer> t1 =new BST();
+        t1.add(8);
+        t1.add(5);
+        t1.add(10);
+        t1.add(6);
+        t1.add(9);
+        t1.add(12);
+        t1.inorder();
+        System.out.println(t1.contains(7));
+        t1.delete(9);
+        t1.inorder();
+        System.out.println();
+        t1.preorder(t1.getRoot());
+        System.out.println();
+        //t1.preorder1();
+    }
+}
