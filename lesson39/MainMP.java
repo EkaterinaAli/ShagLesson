82ed8dbceea7de2f6fc6fd8ee11a7cc497d11b7e
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-12-20 21:21:21 +0300

    Add files via upload

--- lesson39/MainMP.java
+++ lesson39/MainMP.java
@@ -17,84 +17,91 @@ public class MainMP {
         }         */
         int v2 = (new Random()).nextInt(10);
         MyThread2 t2 = new MyThread2(v2);
-        //t2.setDaemon(true);
+        t2.setDaemon(true);
         int v3 = (new Random()).nextInt(10);
         MyThread2 t3 = new MyThread2(v3);
-        //t3.setDaemon(true);
-        //MyThread3 r4 = new MyThread3(v3);
-        //Thread t4 = new Thread(r4);
+       // t3.setDaemon(true);
+        MyThread3 r4 = new MyThread3(v3);
+        Thread t4 = new Thread(r4);
         t2.start();
         t3.start();
-       // t4.start();
+        //t4.start();
+        try {
             t2.join(10000);
+        } catch (InterruptedException ex) {
         }
-        catch (InterruptedException ex){}
-        while (true){
+
+        while (true) {
             System.out.print('@');
         }
-
-
-    }
-
-}
-class MyThread1 extends Thread{
-    @Override
-    public void run(){
-        while (true){
-        System.out.print('A');}
     }
 }
-class  MyThread2 extends Thread{
-    private int value;
 
-    public MyThread2(int value) {
-        this.value = value;
-    }
-    @Override
-    public void run(){
-        while (value>=0){
-            System.out.println("*** The thread " + Thread.currentThread().getName() +
-                    " started with " + value);
-            while ( this.value >=0){
-                System.out.println("From " + Thread.currentThread().getName() +
-                        " value=" + value);
-                value-=1;
-                try {
-                    Thread.sleep(300);}
-                catch (InterruptedException ex){
-
+        class MyThread1 extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.print('A');
                 }
             }
         }
-        System.out.println("*** The thread  " + Thread.currentThread().getName()
-                + " has finished");
-    }
+        class MyThread2 extends Thread {
+            private int value;
 
-}
-class  MyThread3 implements Runnable{
-    private int value;
-
-    public MyThread3(int value) {
-        this.value = value;
-    }
-    @Override
-    public void run(){
-        while (value>=0){
-            System.out.println("*** The thread " + Thread.currentThread().getName() +
-                    " started with " + value);
-            while ( this.value >=0){
-                System.out.println("From " + Thread.currentThread().getName() +
-                        " value=" + value);
-                value-=1;
-                try {
-                    Thread.sleep(300);}
-                catch (InterruptedException ex){
-
-                }
+            public MyThread2(int value) {
+                this.value = value;
             }
-        }
-        System.out.println("*** The thread  " + Thread.currentThread().getName()
-                + " has finished");
-    }
 
-}
+            @Override
+            public void run() {
+                while (value >= 0) {
+                    System.out.println("*** The thread " + Thread.currentThread().getName() +
+                            " started with " + value);
+                    while (this.value >= 0) {
+                        System.out.println("From " + Thread.currentThread().getName() +
+                                " value=" + value);
+                        value -= 1;
+                        try {
+                            Thread.sleep(300);
+                        } catch (InterruptedException ex) {
+
+                        }
+                    }
+                }
+                System.out.println("*** The thread  " + Thread.currentThread().getName()
+                        + " has finished");
+            }
+
+        }
+        class MyThread3 implements Runnable {
+            private int value;
+
+            public MyThread3(int value) {
+                this.value = value;
+            }
+
+            @Override
+            public void run() {
+                while (value >= 0) {
+                    System.out.println("*** The thread " + Thread.currentThread().getName() +
+                            " started with " + value);
+                    while (this.value >= 0) {
+                        System.out.println("From " + Thread.currentThread().getName() +
+                                " value=" + value);
+                        value -= 1;
+                        try {
+                            Thread.sleep(300);
+                        } catch (InterruptedException ex) {
+
+                        }
+                    }
+                }
+                System.out.println("*** The thread  " + Thread.currentThread().getName()
+                        + " has finished");
+            }
+
+        }
+
+
+
+
